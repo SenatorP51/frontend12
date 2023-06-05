386ad42cfd237965efd7a16dabc1f4d64f94f5c3
spglab <37847228+SenatorP51@users.noreply.github.com>
2023-06-05 13:49:55 +0200

    Inicjalizacja reponew10

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          pon 9.00 <code>src/App.js</code> and save to reload.
+          pon 15.00 !!!! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
